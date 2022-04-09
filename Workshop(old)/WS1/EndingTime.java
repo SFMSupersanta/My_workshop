526234cca293d256d03a8a693634061d1883c8ca
SFMSupersanta <otakuaria4710@gmail.com>
2022-04-09 23:06:20 +0700

    wsnew1 update

---
+++ Workshop(old)/WS1/EndingTime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class EndingTime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Starting hour: ");
+        int hour = in.nextInt();
+        System.out.println("Sarting Minues:");
+        int minues = in.nextInt();
+        System.out.println("Lasting time (minues):");
+        int last = in.nextInt();
+        close(in);
+        int status = 0, overtime;
+        if((last % 60) + minues >= 60) 
+        {
+            status = 1;
+            overtime = ((minues + last) % 60);
+        } else overtime = (minues + last)%60;
+        System.out.println("if an event starts at " + hour + " " + minues + " and lasts " + last + " minutes, it will end at " + (hour + (last / 60) + status) % 24 + " " + overtime);
+    }
+}
---
+++ Workshop(old)/WS1/NameMaker.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NameMaker {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your first name: ");
+        String firstName = in.nextLine();
+        System.out.print("Enter your middle name: ");
+        String middleName = in.nextLine();
+        System.out.print("Enter your last name: ");
+        String lastName = in.nextLine();
+        System.out.println("Hello" + firstName + " " + middleName + " " + lastName);
+    }
+}
